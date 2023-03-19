568b2735d412fdf92691d306a0741703a7b110c8
juliphy <96592869+juliphy@users.noreply.github.com>
2023-03-19 13:39:24 +0100

    Go

--- assets/js/values.js
+++ assets/js/values.js
@@ -1,19 +1,18 @@
-getinf = fetch('https://192.168.112.46:3132/api?id=w0n')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    return data
-  });
+fetch('http://127.0.0.1:3132/tele?chatID=490449409')
+.then((response) => response.json())
+.then((data) => {
+    console.log(data[0]);
+    obj = data[0];
+    var fullname = obj.name;
+});
 
-inf = getinf
 // Активировать документы (true - включены, false - отключены)
 var isRightsEnabled = false; // Права
 var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
 var birthdate = "28.12.2003"; // Дата рождения
-var fullname = inf.fullname
+var fullname = 'Piska rqrqrqr Sdasda'
 var name = "Олександр" // Имя
 
 // Паспорт
@@ -31,3 +30,4 @@ var rights_id = "KBE968639" // Права "Серія та номер""
 // COVID-сертификат
 var covid_valid_until = "01.01.2022" // Сертификат "Дійсний до"
 var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
+
