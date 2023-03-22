c5310f03e7a7fff00efb057bca75d31585cd4c23
juliphy <96592869+juliphy@users.noreply.github.com>
2023-03-22 20:41:51 +0100

    Summit

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,24 @@ formElement.addEventListener('submit', (e) => {
   e.preventDefault();
   const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
   // теперь можно извлечь данные
+
   const id = formData.get('id'); // 'John'
-  window.location = window.location.href + 'diia.html?id=' + id;
+
+  fetch('http://127.0.0.1:3132/exist?id='+ id)
+  .then(response => {
+    
+    if (response.status == 200) { 
+      if (window.location.href.slice(window.location.href.length - 10) == 'index.html') { 
+        locationWithoutIndex = window.location.href.slice(0,-10)
+        window.location = locationWithoutIndex + 'diia.html?id=' + id;
+      } else { 
+        window.location = window.location.href + 'diia.html?id=' + id;
+      }
+    } else { 
+      var div = document.createElement('div');
+      div.innerHTML = "Помилка. Введено неправильний ID.";
+      document.body.appendChild(div);
+    }
+    
+  })
 });
